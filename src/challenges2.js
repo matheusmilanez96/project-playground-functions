1a73e8494c9947a05880af7973f282eceb6e25f0
Matheus Milanez <mattmila96@gmail.com>
2022-10-27 17:35:08 -0300

    começando 11 e 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,14 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  for (let i in array) {
+    if (array[i] > 9 || array[i] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
+  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
 }
 
 // Desafio 12
