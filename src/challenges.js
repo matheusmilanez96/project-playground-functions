62258c33d142cad0b305def46ce7deecf1a577c8
Matheus Milanez <mattmila96@gmail.com>
2022-10-27 15:47:05 -0300

    função catAndMouse

--- src/challenges.js
+++ src/challenges.js
@@ -43,8 +43,16 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = Math.abs(mouse - cat1);
+  let dist2 = Math.abs(mouse - cat2);
+  if (dist1 < dist2) {
+    return `cat1`
+  } else if (dist1 > dist2) {
+    return `cat2`
+  } else {
+    return `os gatos trombam e o rato foge`
+  }
 }
 
 // Desafio 8
